openblockee/openblock-ee-desktop

add license of openblockee

src/renderer/license.jsx
@@ -13,7 +13,7 @@ const copyrightInformations = [
         id: 'OpenBlock.cc',
         logo: OpenBlockLogo,
         link: 'https://www.openblock.cc/',
-        license: 'MIT'
+        license: 'EULA'
     },
     {
         id: 'Scratch Foundation',
@@ -24,6 +24,115 @@ const copyrightInformations = [
 ];
 
 const licenseContent = {
+    'EULA': (
+        <div className={styles.licenseContent}>
+            <p>
+                This End-User License Agreement (&quot;EULA&quot;) is a legal agreement
+                between you and OpenBlock (ShenZheng) Technology Co., Ltd..
+                Our EULA was created by for OpenBlockDesktopEE.
+            </p>
+            <p>
+                This EULA agreement governs your acquisition and use of our OpenBlockDesktopEE
+                software (&quot;Software&quot;) directly from OpenBlock (ShenZheng) Technology Co., Ltd.
+                or indirectly through a OpenBlock (ShenZheng) Technology Co., Ltd.
+                authorized reseller or distributor (a &quot;Reseller&quot;).
+            </p>
+            <p>
+                Please read this EULA agreement carefully before completing the
+                installation process and using the OpenBlockDesktopEE software.
+                It provides a license to use the OpenBlockDesktopEE software
+                and contains warranty information and liability disclaimers.
+            </p>
+            <p>
+                If you are entering into this EULA agreement on behalf of a company or
+                other legal entity, you represent that you have the authority to
+                bind such entity and its affiliates to these terms and conditions.
+                If you do not have such authority or if you do not agree with the terms
+                and conditions of this EULA agreement, do not install or use the Software,
+                and you must not accept this EULA agreement.
+            </p>
+            <p>
+                This EULA agreement shall apply only to the Software supplied by
+                OpenBlock (ShenZheng) Technology Co., Ltd.
+                herewith regardless of whether other software is referred to or described herein.
+                The terms also apply to any OpenBlock (ShenZheng) Technology Co., Ltd.
+                updates, supplements, Internet-based services, and support services for
+                the Software, unless other terms accompany those items on delivery.
+                If so, those terms apply.
+            </p>
+            <h3>License Grant</h3>
+            <p>
+                OpenBlock (ShenZheng) Technology Co., Ltd. hereby grants you a personal,
+                non-transferable, non-exclusive licence to use the OpenBlockDesktopEE
+                software on your devices in accordance with the terms of this EULA agreement.
+            </p>
+            <p>
+                You are permitted to load the OpenBlockDesktopEE software (for example a PC,
+                laptop, mobile or tablet) under your control. You are responsible for
+                ensuring your device meets the minimum requirements of
+                the OpenBlockDesktopEE software.
+            </p>
+            <p>
+                You are not permitted to:
+            </p>
+            <ul>
+                <li>
+                    Edit, alter, modify, adapt, translate or otherwise change the whole or
+                    any part of the Software nor permit the whole or any part of
+                    the Software to be combined with or become incorporated in any other software,
+                    nor decompile, disassemble or reverse engineer the
+                    Software or attempt to do any such things
+                </li>
+                <li>
+                    Reproduce, copy, distribute, resell or otherwise use the Software for any
+                    commercial purpose
+                </li>
+                <li>
+                    Allow any third party to use the Software on behalf of or for the benefit
+                    of any third party
+                </li>
+                <li>
+                    Use the Software in any way which breaches any applicable local,
+                    national or international law
+                </li>
+                <li>
+                    use the Software for any purpose that OpenBlock (ShenZheng) Technology Co., Ltd.
+                    considers is a breach of this EULA agreement
+                </li>
+            </ul>
+            <h3>Intellectual Property and Ownership</h3>
+            <p>
+                OpenBlock (ShenZheng) Technology Co., Ltd. shall at all times retain ownership
+                of the Software as originally downloaded by you and all subsequent downloads
+                of the Software by you. The Software (and the copyright, and other intellectual
+                property rights of whatever nature in the Software,
+                including any modifications made thereto) are and shall remain the property of
+                OpenBlock (ShenZheng) Technology Co., Ltd..
+            </p>
+            <p>
+                OpenBlock (ShenZheng) Technology Co., Ltd. reserves the right to grant licences to
+                use the Software to third parties.
+            </p>
+            <h3>Termination</h3>
+            <p>
+                This EULA agreement is effective from the date you first use the Software and
+                shall continue until terminated. You may terminate it at any time upon written
+                notice to OpenBlock (ShenZheng) Technology Co., Ltd..
+            </p>
+            <p>
+                It will also terminate immediately if you fail to comply with any term of this
+                EULA agreement. Upon such termination, the licenses granted by this EULA agreement
+                will immediately terminate and you agree to stop all access and use of the Software.
+                The provisions that by their nature continue and survive will survive any
+                termination of this EULA agreement.
+            </p>
+            <h3>Governing Law</h3>
+            <p>
+                This EULA agreement, and any dispute arising out of or in connection with this
+                EULA agreement, shall be governed by and construed in accordance with the laws of China.
+            </p>
+        </div>
+    ),
     'MIT': (
         <div className={styles.licenseContent}>
             <p>

openblockee/openblock-distro-desktop

src/renderer/license.jsx
@@ -13,7 +13,7 @@ const copyrightInformations = [
         id: 'OpenBlock.cc',
         logo: OpenBlockLogo,
         link: 'https://www.openblock.cc/',
-        license: 'MIT'
+        license: 'EULA'
     },
     {
         id: 'Scratch Foundation',
@@ -24,6 +24,119 @@ const copyrightInformations = [
 ];
 
 const licenseContent = {
+    'EULA': (
+        <div className={styles.licenseContent}>
+            <p>
+                This End-User License Agreement (&quot;EULA&quot;) is a legal agreement
+                between you and OpenBlock (ShenZhen) Technology Co., Ltd..
+            </p>
+            <p>
+                This EULA agreement governs your acquisition and use of our OpenBlockDesktopEE
+                software (&quot;Software&quot;) directly from OpenBlock (ShenZhen) Technology Co., Ltd.
+                or indirectly through a OpenBlock (ShenZhen) Technology Co., Ltd.
+                authorized reseller or distributor (a &quot;Reseller&quot;).
+            </p>
+            <p>
+                Please read this EULA agreement carefully before completing the
+                installation process and using the OpenBlockDesktopEE software.
+                It provides a license to use the OpenBlockDesktopEE software
+                and contains warranty information and liability disclaimers.
+            </p>
+            <p>
+                By clicking &quot;accept&quot; or installing and/or using the OpenBlockDesktopEE
+                software, you are confirming your acceptance of the Software and agreeing to
+                become bound by the terms of this EULA agreement.
+            </p>
+            <p>
+                If you are entering into this EULA agreement on behalf of a company or
+                other legal entity, you represent that you have the authority to
+                bind such entity and its affiliates to these terms and conditions.
+                If you do not have such authority or if you do not agree with the terms
+                and conditions of this EULA agreement, do not install or use the Software,
+                and you must not accept this EULA agreement.
+            </p>
+            <p>
+                This EULA agreement shall apply only to the Software supplied by
+                OpenBlock (ShenZhen) Technology Co., Ltd.
+                herewith regardless of whether other software is referred to or described herein.
+                The terms also apply to any OpenBlock (ShenZhen) Technology Co., Ltd.
+                updates, supplements, Internet-based services, and support services for
+                the Software, unless other terms accompany those items on delivery.
+                If so, those terms apply.
+            </p>
+            <h3>License Grant</h3>
+            <p>
+                OpenBlock (ShenZhen) Technology Co., Ltd. hereby grants you a personal,
+                non-transferable, non-exclusive licence to use the OpenBlockDesktopEE
+                software on your devices in accordance with the terms of this EULA agreement.
+            </p>
+            <p>
+                You are permitted to load the OpenBlockDesktopEE software (for example a PC,
+                laptop, mobile or tablet) under your control. You are responsible for
+                ensuring your device meets the minimum requirements of
+                the OpenBlockDesktopEE software.
+            </p>
+            <p>
+                You are not permitted to:
+            </p>
+            <ul>
+                <li>
+                    Edit, alter, modify, adapt or otherwise change the whole or
+                    any part of the Software nor permit the whole or any part of
+                    the Software to be combined with or become incorporated in any other software,
+                    nor decompile, disassemble or reverse engineer the
+                    Software or attempt to do any such things
+                </li>
+                <li>
+                    Reproduce, copy, distribute, resell or otherwise use the Software for any
+                    commercial purpose
+                </li>
+                <li>
+                    Allow any third party to use the Software on behalf of or for the benefit
+                    of any third party
+                </li>
+                <li>
+                    Use the Software in any way which breaches any applicable local,
+                    national or international law
+                </li>
+                <li>
+                    use the Software for any purpose that OpenBlock (ShenZhen) Technology Co., Ltd.
+                    considers is a breach of this EULA agreement
+                </li>
+            </ul>
+            <h3>Intellectual Property and Ownership</h3>
+            <p>
+                OpenBlock (ShenZhen) Technology Co., Ltd. shall at all times retain ownership
+                of the Software as originally downloaded by you and all subsequent downloads
+                of the Software by you. The Software (and the copyright, and other intellectual
+                property rights of whatever nature in the Software,
+                including any modifications made thereto) are and shall remain the property of
+                OpenBlock (ShenZhen) Technology Co., Ltd..
+            </p>
+            <p>
+                OpenBlock (ShenZhen) Technology Co., Ltd. reserves the right to grant licences to
+                use the Software to third parties.
+            </p>
+            <h3>Termination</h3>
+            <p>
+                This EULA agreement is effective from the date you first use the Software and
+                shall continue until terminated. You may terminate it at any time upon written
+                notice to OpenBlock (ShenZhen) Technology Co., Ltd..
+            </p>
+            <p>
+                It will also terminate immediately if you fail to comply with any term of this
+                EULA agreement. Upon such termination, the licenses granted by this EULA agreement
+                will immediately terminate and you agree to stop all access and use of the Software.
+                The provisions that by their nature continue and survive will survive any
+                termination of this EULA agreement.
+            </p>
+            <h3>Governing Law</h3>
+            <p>
+                This EULA agreement, and any dispute arising out of or in connection with this
+                EULA agreement, shall be governed by and construed in accordance with the laws of China.
+            </p>
+        </div>
+    ),
     'MIT': (
         <div className={styles.licenseContent}>
             <p>